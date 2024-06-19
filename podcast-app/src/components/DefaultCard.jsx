e850995e68b806cfe004afce156b58acc605fdd2
Rori <rbmasilo@gmail.com>
2024-06-19 20:23:03 +0200

    Updated DefaultCard component with new styles and layout

--- podcast-app/src/components/DefaultCard.jsx
+++ podcast-app/src/components/DefaultCard.jsx
@@ -7,6 +7,16 @@ const Card = styled.div`
   background-color: #4b7f12;
   border-radius: 0.6rem;
   padding: 1rem;
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-8px);
+    transition: all 0.4s ease-in-out;
+    box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
+    filter: brightness(1.3);
+  }
+  @media (max-width: 768px) {
+    width: 250px;
+  }
 `;
 
 const DefaultCardText = styled.div`
@@ -15,13 +25,27 @@ const DefaultCardText = styled.div`
   font-weight: 600;
 `;
 
-const DefaultCardImg = styled.div``;
-
+const DefaultCardImg = styled.div`
+  height: 90px;
+  width: 80px;
+  object-fit: cover;
+  clip-path: polygon(0 0, 100% 0, 100% 66%, 0 98%);
+  transform: rotate(20deg);
+`;
+const FlexContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
 export const DefaultCard = () => {
   return (
-    <Card>
-      <DefaultCardText>Podcasts</DefaultCardText>
-      <DefaultCardImg></DefaultCardImg>
+    <Card style={{ "background-color": `${category.color}` }}>
+      <DefaultCardText>{category.name}</DefaultCardText>
+      <FlexContainer>
+        <DefaultCardImg src={category.img} alt="podcast-image" />
+      </FlexContainer>
     </Card>
   );
 };
