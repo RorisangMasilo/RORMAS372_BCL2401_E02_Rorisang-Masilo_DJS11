446f76d3d188c461df716b7b5e62372258b35c7d
Rori <rbmasilo@gmail.com>
2024-06-19 19:17:39 +0200

    Added DefaultCard component with Card, DefaultCardText, and DefaultCardImg elements

--- podcast-app/src/components/DefaultCard.jsx
+++ podcast-app/src/components/DefaultCard.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Card = styled.div`
-width:200px;
-height:200px;
-background-color:#4B7F12;
-border-radius:0.6rem;
-padding:1rem;
+  width: 200px;
+  height: 200px;
+  background-color: #4b7f12;
+  border-radius: 0.6rem;
+  padding: 1rem;
+`;
+const DefaultCardText = styled.div``;
+const DefaultCardImg = styled.div``;
+
+export const DefaultCard = () => {
+  return (
+    <Card>
+      <DefaultCardText>Podcasts</DefaultCardText>
+      <DefaultCardImg></DefaultCardImg>
+    </Card>
+  );
+};
