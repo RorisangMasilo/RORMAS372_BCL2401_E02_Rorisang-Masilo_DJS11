cddebae962bd1fc4529e4b967c0a3047b71b4cc9
Rori <rbmasilo@gmail.com>
2024-06-20 18:23:27 +0200

    Added styled components for SearchedCard and PodcastImage in TopResult component

--- podcast-app/src/components/TopResult.jsx
+++ podcast-app/src/components/TopResult.jsx
@@ -3,6 +3,36 @@ import styled from "styled-components";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 
+const SearchedCard = styled(Link)`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  padding: 18px 18px 30px 18px;
+  border-radius: 6px;
+  gap: 12px;
+  background-color: ${({ theme }) => theme.card};
+  box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  text-decoration: none;
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-8px);
+    transition: all 0.4s ease-in-out;
+    box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
+    filter: brightness(1.3);
+  }
+  @media (max-width: 768px) {
+    width: 290px;
+  }
+`;
+
+const PodcastImage = styled.img`
+  object-fit: cover;
+  width: 50%;
+  border-radius: 6px;
+  box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.3);
+`;
+
 const TopResult = ({ podcast }) => {
   return (
     <SearchedCard to={`/podcast/${podcast?._id}`}>
