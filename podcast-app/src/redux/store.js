43b5d918a918cd6c1b15c4d0a1742b12017251d7
Rori <rbmasilo@gmail.com>
2024-06-21 10:17:19 +0200

    Refactor store.js to use @reduxjs/toolkit and redux-persist

--- podcast-app/src/redux/store.js
+++ podcast-app/src/redux/store.js
@@ -1,6 +1,49 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers"; // Assuming you have a rootReducer
+import {
+  configureStor,
+  combineReducers,
+  configureStore,
+} from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import snackbarSlice from "./snackbarSlice";
+import audioReducer from "./audioplayerSlice";
+import signinReducer from "./setSigninSlice";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { version } from "react";
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
-export default store;
+const rootReducer = combineReducers({
+  user: userReducer,
+  snackbar: snackbarReducer,
+  audioplayer: audioReducer,
+  signin: signinReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializeCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
