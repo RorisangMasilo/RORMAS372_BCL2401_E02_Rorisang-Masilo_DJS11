0a5f159ed99db37dc9c65a61abeaf2f2078007f8
Rori <rbmasilo@gmail.com>
2024-06-21 11:01:02 +0200

    Refactored App.js: removed unused imports, simplified component structure, and cleaned up code organization.

--- podcast-app/src/App.js
+++ podcast-app/src/App.js
@@ -1,141 +1,74 @@
-import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
-import { darkTheme, lightTheme } from "./utils/Themes.js";
-import Signup from "../src/components/Signup.jsx";
-import Signin from "../src/components/Signin.jsx";
-import OTP from "../src/components/OTP.jsx";
-import Navbar from "../src/components/Navbar.jsx";
-import Menu from "../src/components/Menu.jsx";
-import Dashboard from "../src/pages/Dashboard.jsx";
-import ToastMessage from "./components/ToastMessage.jsx";
-import Search from "../src/pages/Search.jsx";
-import Favourites from "../src/pages/Favourites.jsx";
-import Profile from "../src/pages/Profile.jsx";
-import Upload from "../src/components/Upload.jsx";
-import DisplayPodcasts from "../src/pages/DisplayPodcasts.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
-import AudioPlayer from "./components/AudioPlayer.jsx";
-import VideoPlayer from "./components/VideoPlayer.jsx";
-import PodcastDetails from "./pages/PodcastDetails.jsx";
-import { closeSignin } from "./redux/setSigninSlice.jsx";
+import React, { useState, BrowserRouter, Routes, Route } from "react";
+import styled, { themeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+import Menu from "./components/Menu";
+import NavBar from "./components/NavBar";
+import Dashboard from "./pages/Dashboard";
+import Search from "./pages/Search";
+import DisplayPodcast from "./pages/DisplayPodcast";
+import Favourites from "./pages/Favourite";
+import PodcastDetails from "./pages/PodcastDetails";
+import Profile from "./pages/profile";
 
+const Container = styled.div`
+  display: flex;
+  background: ${({ theme }) => theme.bgLight};
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
 const Frame = styled.div`
   display: flex;
   flex-direction: column;
   flex: 3;
 `;
 
-const Podstream = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 100vh;
-  background: ${({ theme }) => theme.bgLight};
-  overflow-y: hidden;
-  overflow-x: hidden;
-`;
-
 function App() {
+  // Hooks
   const [darkMode, setDarkMode] = useState(true);
-  const { open, message, severity } = useSelector((state) => state.snackbar);
-  const { openplayer, type, episode, podid, currenttime, index } = useSelector(
-    (state) => state.audioplayer
-  );
-  const { opensi } = useSelector((state) => state.signin);
-  const [SignUpOpen, setSignUpOpen] = useState(false);
-  const [SignInOpen, setSignInOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(true);
-  const [uploadOpen, setUploadOpen] = useState(false);
-
-  const { currentUser } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  //set the menuOpen state to false if the screen size is less than 768px
-  useEffect(() => {
-    const resize = () => {
-      if (window.innerWidth < 1110) {
-        setMenuOpen(false);
-      } else {
-        setMenuOpen(true);
-      }
-    };
-    resize();
-    window.addEventListener("resize", resize);
-    return () => window.removeEventListener("resize", resize);
-  }, []);
-
-  useEffect(() => {
-    dispatch(closeSignin());
-  }, []);
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+    <themeProvider theme={darkMode ? darkTheme : lightTheme}>
       <BrowserRouter>
-        {opensi && (
-          <Signin setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />
-        )}
-        {SignUpOpen && (
-          <Signup setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />
-        )}
-        {uploadOpen && <Upload setUploadOpen={setUploadOpen} />}
-        {openplayer && type === "video" && (
-          <VideoPlayer
-            episode={episode}
-            podid={podid}
-            currenttime={currenttime}
-            index={index}
-          />
-        )}
-        {openplayer && type === "audio" && (
-          <AudioPlayer
-            episode={episode}
-            podid={podid}
-            currenttime={currenttime}
-            index={index}
-          />
-        )}
-        <Podstream>
+        <Container>
           {menuOpen && (
             <Menu
-              setMenuOpen={setMenuOpen}
-              darkMode={darkMode}
-              setDarkMode={setDarkMode}
-              setUploadOpen={setUploadOpen}
-              setSignInOpen={setSignInOpen}
-            />
-          )}
-          <Frame>
-            <Navbar
               menuOpen={menuOpen}
               setMenuOpen={setMenuOpen}
-              setSignInOpen={setSignInOpen}
-              setSignUpOpen={setSignUpOpen}
-            />
+              setDarkMode={setDarkMode}
+              darkMode={darkMode}
+            ></Menu>
+          )}
+          <Frame>
+            <NavBar>
+              menuOpen={menuOpen}
+              setMenuOpen={setMenuOpen}
+              setDarkMode={setDarkMode}
+              darkMode={darkMode}
+            </NavBar>
             <Routes>
+              <Route path="/" exact element={<Dashboard />}></Route>
+              <Route path="/search" exact element={<Search />}></Route>
+              <Route path="/Profile" exact element={<Profile />}></Route>
+              <Route path="/Favourites" exact element={<Favourites />}></Route>
               <Route
-                path="/"
+                path="/podcast/:id"
                 exact
-                element={<Dashboard setSignInOpen={setSignInOpen} />}
-              />
-              <Route path="/search" exact element={<Search />} />
-              <Route path="/favourites" exact element={<Favourites />} />
-              <Route path="/profile" exact element={<Profile />} />
-              <Route path="/podcast/:id" exact element={<PodcastDetails />} />
+                element={<PodcastDetails />}
+              ></Route>
               <Route
                 path="/showpodcasts/:type"
                 exact
-                element={<DisplayPodcasts />}
-              />
+                element={<DisplayPodcast />}
+              ></Route>
             </Routes>
+            PODCAST
           </Frame>
-
-          {open && (
-            <ToastMessage open={open} message={message} severity={severity} />
-          )}
-        </Podstream>
+        </Container>
       </BrowserRouter>
-    </ThemeProvider>
+    </themeProvider>
   );
 }
 
